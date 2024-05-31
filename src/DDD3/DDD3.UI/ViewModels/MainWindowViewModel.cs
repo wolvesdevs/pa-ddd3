9369506847b850d834a4e399239ba2d06ae23469
wolvesdevs <75551104+wolvesdevs@users.noreply.github.com>
2024-05-31 09:57:56 +0900

    refs #003 10. S1_ボタンのデータバインド

--- src/DDD3/DDD3.UI/ViewModels/MainWindowViewModel.cs
+++ src/DDD3/DDD3.UI/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
 
 namespace DDD3.UI.ViewModels;
 public class MainWindowViewModel : BindableBase
@@ -10,8 +12,22 @@ public class MainWindowViewModel : BindableBase
         set => SetProperty(ref _title, value);
     }
 
+    private string _systemDateLabel = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+    public string SystemDateLabel
+    {
+        get => _systemDateLabel;
+        set => SetProperty(ref _systemDateLabel, value);
+    }
+
+    public DelegateCommand SystemDateUpdateButton { get; }
+
     public MainWindowViewModel()
     {
+        SystemDateUpdateButton = new DelegateCommand(SystemDateUpdateButtonExecute);
+    }
 
+    private void SystemDateUpdateButtonExecute()
+    {
+        SystemDateLabel = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
     }
 }
