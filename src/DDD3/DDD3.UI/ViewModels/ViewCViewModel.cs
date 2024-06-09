af9045ca56d9270b1f92baa4372cfa1f4560011f
wolvesdevs <75551104+wolvesdevs@users.noreply.github.com>
2024-06-09 17:42:41 +0900

    refs #014 23. S1_リストボックスのデータバインド

--- src/DDD3/DDD3.UI/ViewModels/ViewCViewModel.cs
+++ src/DDD3/DDD3.UI/ViewModels/ViewCViewModel.cs
@@ -4,20 +4,44 @@ using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 
 namespace DDD3.UI.ViewModels;
 public class ViewCViewModel : BindableBase, IConfirmNavigationRequest
 {
+    #region フィールド
+
     private IMessageService _messageService;
+
+    #endregion
+
+    #region プロパティ
+
+    private ObservableCollection<string> _myListBox = new();
+    public ObservableCollection<string> MyListBox
+    {
+        get => _myListBox;
+        set => SetProperty(ref _myListBox, value);
+    }
+
+    #endregion
+
+    #region コンストラクタ
 
     public ViewCViewModel() : this(new MessageService()) { }
 
     public ViewCViewModel(IMessageService messageService)
     {
         _messageService = messageService;
+
+        MyListBox.Add("AAA");
+        MyListBox.Add("BBB");
+        MyListBox.Add("CCC");
     }
+
+    #endregion
 
     public void ConfirmNavigationRequest(NavigationContext navigationContext, Action<bool> continuationCallback)
     {
