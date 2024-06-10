098c01888dc221fff6e655402f5830021a61f5dc
wolvesdevs <75551104+wolvesdevs@users.noreply.github.com>
2024-06-10 15:39:18 +0900

    refs #024 33. S2_直近画面の実装1

---
+++ src2/DDDNET8/DDDNET8.WPF/ViewModels/LatestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DDDNET8.WPF.ViewModels
+{
+    public class LatestViewModel : BindableBase
+    {
+        public LatestViewModel()
+        {
+
+        }
+    }
+}
---
+++ src2/DDDNET8/DDDNET8.WPF/Views/LatestView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DDDNET8.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for LatestView
+    /// </summary>
+    public partial class LatestView : UserControl
+    {
+        public LatestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
