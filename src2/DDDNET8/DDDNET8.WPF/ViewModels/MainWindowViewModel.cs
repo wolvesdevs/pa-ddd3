a21c8fa7decea4782bb0e395d9dff30ff6d78c2f
wolvesdevs <75551104+wolvesdevs@users.noreply.github.com>
2024-06-10 14:28:23 +0900

    refs #023 32. S2_メイン画面の作成

--- src2/DDDNET8/DDDNET8.WPF/ViewModels/MainWindowViewModel.cs
+++ src2/DDDNET8/DDDNET8.WPF/ViewModels/MainWindowViewModel.cs
@@ -4,11 +4,11 @@ namespace DDDNET8.WPF.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "DDD";
         public string Title
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get => _title;
+            set => SetProperty(ref _title, value);
         }
 
         public MainWindowViewModel()
