25aa23adcf54fe903c0310204f3751a34d4a6b5a
wolvesdevs <75551104+wolvesdevs@users.noreply.github.com>
2024-06-11 13:48:41 +0900

    refs #033 43. S2_List画面のViewModel置き換え

--- src2/DDDNET8/DDDNET8.WPF/ViewModels/WeatherListViewModel.cs
+++ src2/DDDNET8/DDDNET8.WPF/ViewModels/WeatherListViewModel.cs
@@ -1,16 +1,32 @@
-﻿using Prism.Commands;
+﻿using DDDNET8.Domain.Repositories;
+using DDDNET8.Infrastructure.SqlServer;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace DDDNET8.WPF.ViewModels
 {
     public class WeatherListViewModel : BindableBase
     {
-        public WeatherListViewModel()
-        {
+        private IWeatherRepository _weather;
 
+        private ObservableCollection<WeatherListViewModelWeather> _weathers = new();
+        public ObservableCollection<WeatherListViewModelWeather> Weathers
+        {
+            get => _weathers;
+            set => SetProperty(ref _weathers, value);
         }
+
+        #region コンストラクタ
+
+        public WeatherListViewModel() : this(new WeatherSqlServer()) { }
+
+        public WeatherListViewModel(IWeatherRepository weather)
+        {
+            _weather = weather;
+            Weathers = new(_weather.GetData().Select(entity => new WeatherListViewModelWeather(entity)).ToList());
+        }
+
+        #endregion
     }
 }
